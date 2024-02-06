e54b7f1bdf8a3354eee0d112ccf53929b26e40f2
Zack Guard <zacharyguard@gmail.com>
2024-02-06 10:15:46 +0000

    Support archaic Nims

--- tests/test_skipHook.nim
+++ tests/test_skipHook.nim
@@ -14,7 +14,7 @@ type
 proc skipHook(T: typedesc[Foo], key: static string): bool =
   key in ["password", "conn"]
 
-proc skipHook[V, E](_: typedesc[V], _: static string, value: Option[E]): bool =
+proc skipHook[E](T: typedesc, key: static string, value: Option[E]): bool =
   value.isNone
 
 let v = Foo(a:1, password: "12345", b:0.6, conn: Conn(id: 1), d: some("hello"))
